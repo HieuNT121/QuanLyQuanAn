d6f82a112acc62dc9f36ea7da69ec7400bd0e359
HieuNT121 <138646231+HieuNT121@users.noreply.github.com>
2024-01-03 23:57:30 +0700

    hoàn thiện quản trị hệ thống

---
+++ QuanLyQuanAn/FrmQuanLyTaiKhoan.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace QuanLyQuanAn
+{
+    partial class FrmQuanLyTaiKhoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTenTaiKhoan = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbTenTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txbMatKhau = new System.Windows.Forms.TextBox();
+            this.dtvgUser = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.cbxLoaiTaiKhoan = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvgUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTenTaiKhoan
+            // 
+            this.lbTenTaiKhoan.AutoSize = true;
+            this.lbTenTaiKhoan.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenTaiKhoan.Location = new System.Drawing.Point(55, 87);
+            this.lbTenTaiKhoan.Name = "lbTenTaiKhoan";
+            this.lbTenTaiKhoan.Size = new System.Drawing.Size(149, 31);
+            this.lbTenTaiKhoan.TabIndex = 0;
+            this.lbTenTaiKhoan.Text = "Tên tài khoản";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mật khẩu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Loại tài khoản";
+            // 
+            // txbTenTaiKhoan
+            // 
+            this.txbTenTaiKhoan.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTenTaiKhoan.Location = new System.Drawing.Point(235, 84);
+            this.txbTenTaiKhoan.Name = "txbTenTaiKhoan";
+            this.txbTenTaiKhoan.Size = new System.Drawing.Size(224, 38);
+            this.txbTenTaiKhoan.TabIndex = 3;
+            // 
+            // txbMatKhau
+            // 
+            this.txbMatKhau.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMatKhau.Location = new System.Drawing.Point(235, 162);
+            this.txbMatKhau.Name = "txbMatKhau";
+            this.txbMatKhau.Size = new System.Drawing.Size(224, 38);
+            this.txbMatKhau.TabIndex = 4;
+            // 
+            // dtvgUser
+            // 
+            this.dtvgUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvgUser.Location = new System.Drawing.Point(496, 84);
+            this.dtvgUser.MultiSelect = false;
+            this.dtvgUser.Name = "dtvgUser";
+            this.dtvgUser.ReadOnly = true;
+            this.dtvgUser.RowHeadersWidth = 51;
+            this.dtvgUser.RowTemplate.Height = 24;
+            this.dtvgUser.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dtvgUser.Size = new System.Drawing.Size(443, 444);
+            this.dtvgUser.TabIndex = 6;
+            this.dtvgUser.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtvgUser_CellContentClick);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(61, 387);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(87, 43);
+            this.btnThem.TabIndex = 7;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(200, 387);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(87, 43);
+            this.btnSua.TabIndex = 8;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(61, 485);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(87, 43);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(200, 485);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(87, 43);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // cbxLoaiTaiKhoan
+            // 
+            this.cbxLoaiTaiKhoan.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxLoaiTaiKhoan.FormattingEnabled = true;
+            this.cbxLoaiTaiKhoan.Location = new System.Drawing.Point(235, 234);
+            this.cbxLoaiTaiKhoan.Name = "cbxLoaiTaiKhoan";
+            this.cbxLoaiTaiKhoan.Size = new System.Drawing.Size(224, 39);
+            this.cbxLoaiTaiKhoan.TabIndex = 11;
+            // 
+            // FrmQuanLyTaiKhoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 563);
+            this.Controls.Add(this.cbxLoaiTaiKhoan);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dtvgUser);
+            this.Controls.Add(this.txbMatKhau);
+            this.Controls.Add(this.txbTenTaiKhoan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbTenTaiKhoan);
+            this.Name = "FrmQuanLyTaiKhoan";
+            this.Text = "FrmQuanLyTaiKhoan";
+            this.Load += new System.EventHandler(this.FrmQuanLyTaiKhoan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtvgUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTenTaiKhoan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbTenTaiKhoan;
+        private System.Windows.Forms.TextBox txbMatKhau;
+        private System.Windows.Forms.DataGridView dtvgUser;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ComboBox cbxLoaiTaiKhoan;
+    }
+}
