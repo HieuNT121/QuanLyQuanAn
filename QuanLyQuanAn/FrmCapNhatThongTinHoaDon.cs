e0356318adb4a12e6c0ff99e77fc6ccbc2343d27
NormieAhihi <127420196+NormieAhihi@users.noreply.github.com>
2024-01-09 11:17:55 +0700

    Update FrmCapNhatThongTinHoaDon.cs

--- QuanLyQuanAn/FrmCapNhatThongTinHoaDon.cs
+++ QuanLyQuanAn/FrmCapNhatThongTinHoaDon.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,64 @@ namespace QuanLyQuanAn
 {
     public partial class FrmCapNhatThongTinHoaDon : Form
     {
+        SqlConnection connection;
+        SqlCommand command;
+        string str = @"Data Source=DESKTOP-MC\SQLEXPRESS;Initial Catalog=QuanLyQuanAn;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+        void loadData()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select * from ThongTinHoaDon";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dtgvThongTinHoaDon.DataSource = table;
+        }
         public FrmCapNhatThongTinHoaDon()
         {
             InitializeComponent();
+        }
+
+        private void dtgvThongTinHoaDon_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dtgvThongTinHoaDon.CurrentRow.Index;
+            tbMaHoaDon.Text = dtgvThongTinHoaDon.Rows[i].Cells[0].Value.ToString();
+            tbMaMonAn.Text = dtgvThongTinHoaDon.Rows[i].Cells[1].Value.ToString();
+            cbSoLuong.Text = dtgvThongTinHoaDon.Rows[i].Cells[2].Value.ToString();
+        }
+
+        private void btXem_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(str);
+            connection.Open();
+            loadData();
+        }
+
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "insert into ThongTinHoaDon values ('" + tbMaHoaDon.Text + "', '" + tbMaMonAn.Text + "', '"+cbSoLuong.Text+"')";
+            command.ExecuteNonQuery();
+            loadData();
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "delete from ThongTinHoaDon where MaHoaDon = '" + tbMaHoaDon.Text + "' ";
+            command.ExecuteNonQuery();
+            loadData();
+        }
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "update ThongTinHoaDon set MaMonAn = '" + tbMaMonAn.Text + "', SoLuong = '" + cbSoLuong.Text + "' where MaHoaDon = '"+tbMaHoaDon.Text+"' ";
+            command.ExecuteNonQuery();
+            loadData();
         }
     }
 }
